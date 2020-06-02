e3cc596d25735da2d5f8d6c7a1f6d8c730bc4d8f
Rafal Leszko <rafal@hazelcast.com>
2020-06-02 15:50:26 +0200

    Update build-3.0.sh

--- build-3.0.sh
+++ build-3.0.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 HZ_LICENSE_KEY=${1}
 JET_LICENSE_KEY=${2}
-./build.sh 3.0 rhel 3.12.7 3.12.9 2.9.0 3.12.7 3.12.2 ${HZ_LICENSE_KEY} docker-hub 3.2.2 3.2.2 1.2.2 ${JET_LICENSE_KEY}
+./build.sh 3.0 rhel 3.12.7 3.12.9 2.9.0 3.12.7 3.12.2 ${HZ_LICENSE_KEY} docker-hub 3.2.2 3.2.2 1.2.0 ${JET_LICENSE_KEY}
